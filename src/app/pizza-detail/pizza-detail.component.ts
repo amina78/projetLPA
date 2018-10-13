5b4e90714dec76f955e434fa2b64d25fdf3b27e6
Aminata Ndiaye <andiay01@utbm.fr>
2018-10-13 14:14:49 +0200

    back

--- src/app/pizza-detail/pizza-detail.component.ts
+++ src/app/pizza-detail/pizza-detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PizzaService } from '../pizza.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Pizza } from '../pizza';
 @Component({
   selector: 'app-pizza-detail',
   templateUrl: './pizza-detail.component.html',
   styleUrls: ['./pizza-detail.component.css']
 })
 export class PizzaDetailComponent implements OnInit {
-
-  constructor() { }
+pizza: Pizza
+  constructor( private route: ActivatedRoute,
+    private pizzaService: PizzaService,
+    private location: Location) { }
 
   ngOnInit() {
+this.getPizza()
   }
-
+  getPizza(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pizzaService.getPizza(id)
+      .subscribe(pizza => this.pizza = pizza);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
